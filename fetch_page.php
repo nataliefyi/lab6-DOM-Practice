88b4eef2c23d4fe963b25ca4e4f5d1a855a3dec5
Natalie <56485969+nataliefyi@users.noreply.github.com>
2020-03-03 23:58:14 -0800

    Initial commit - setting up project

---
+++ fetch_page.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Fetch Page</title>
+        <link rel="stylesheet" href="fetch_page.css">
+        <script src="fetch_page.js"></script>
+    </head>
+    <body>
+        <p id="contents">Hello World</p>
+    </body>
+</html>
+
+<?php
